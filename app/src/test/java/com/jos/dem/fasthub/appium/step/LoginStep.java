f48a04eb085c73907c68ea752e468666ac38e9a1
josdem <joseluis.delacruz@gmail.com>
2018-10-24 15:18:47 -0400

    Adding first test definition

---
+++ app/src/test/java/com/jos/dem/fasthub/appium/step/LoginStep.java
@@ -0,0 +1,56 @@
+package com.jos.dem.fasthub.appium.step;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import io.appium.java_client.AppiumDriver;
+
+import com.jos.dem.appium.fasthub.service.LoginService;
+import com.jos.dem.appium.fasthub.service.impl.LoginServiceImpl;
+
+public class CategoryStep {
+
+  private AppiumDriver driver;
+  private DesiredCapabilities capabilities = new DesiredCapabilities();
+  private LoginService loginService = new LoginServiceImpl();
+
+  private Logger log = Logger.getLogger(CategoryStep.class.getName());
+
+  @Before
+  public void setup(){
+    categoryService.setCapabilities(capabilities);
+  }
+
+  @When("I launch the application")
+  public void shouldOpenTheApplication() throws Exception {
+    log.info("Running: I launch the application at " + new Date());
+    driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Then("I should be able to see all login options")
+  public void shouldDisplayCategories() throws Exception {
+    log.info("Running: I should be able to see all login options at " + new Date());
+  }
+
+  @After
+  public void end(){
+    driver.quit();
+  }
+
+}
---
+++ app/src/test/resources/login.feature
@@ -0,0 +1,4 @@
+Feature: User can login in the app
+  Scenario: As a user I should be able to login
+    When I launch the application
+    Then I should be able to see all login options

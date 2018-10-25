b8d8371c461a946fccf13e5381604cdada88bfc9
josdem <joseluis.delacruz@gmail.com>
2018-10-25 16:00:44 -0400

    #2 Adding issue step

---
+++ app/src/test/java/com/jos/dem/fasthub/appium/step/IssueStep.java
@@ -0,0 +1,40 @@
+package com.jos.dem.fasthub.appium.step;
+
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Date;
+import java.util.logging.Logger;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import io.appium.java_client.AppiumDriver;
+
+public class IssueStep extends BaseStep {
+
+  private AppiumDriver driver;
+  private Logger log = Logger.getLogger(this.getClass().getName());
+
+  @When("I click on issues")
+  public void shouldOpenTheApplication() throws Exception {
+    log.info("Running: I launch the application at " + new Date());
+    driver = getDriver();
+    assumeTrue(driver.findElementsByAccessibilityId("com.fastaccess.github:id/bottomNavigation"));
+    //TODO: Send Tap on this element: com.fastaccess.github:id/pinned
+  }
+
+  @Then("I should be able to see issues")
+  public void shouldDisplayCategories() throws Exception {
+    log.info("Running: I should be able to see issues at " + new Date());
+    assumeTrue(driver.findElementsByAccessibilityId("com.fastaccess.github:id/pager") != null);
+
+    log.info("IssueStep: Container exist, let's get at least one issue");
+    assertNotNull(driver.findElementsByAccessibilityId("com.fastaccess.github:id/title"));
+    assertNotNull(driver.findElementsByAccessibilityId("com.fastaccess.github:id/details"));
+  }
+
+}

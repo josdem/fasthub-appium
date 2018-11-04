da68fdb63969bc72caf2a754faa5911861d663d8
josdem <joseluis.delacruz@gmail.com>
2018-11-04 07:40:19 -0500

    #4 Adding configuration reader

---
+++ app/src/main/java/com/jos/dem/fasthub/appium/util/ConfigurationReader.java
@@ -0,0 +1,27 @@
+package com.jos.dem.fasthub.appium.util;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
+public class ConfigurationReader {
+  private static Configurations configurations = new Configurations();
+  private static File file = new File("src/main/res/configuration.properties");
+
+  private static Logger log = Logger.getLogger(ConfigurationReader.class.getName());
+
+  public static String getProperty(String key){
+    String value = "";
+    try{
+      Configuration configuration = configurations.properties(file);
+      value = configuration.getString(key);
+    }catch(ConfigurationException cex){
+      log.warning(cex.getMessage());
+    }
+    return value;
+  }
+
+}

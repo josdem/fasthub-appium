57a7f3dc581627a31b380fdb272459792f12e580
josdem <joseluis.delacruz@gmail.com>
2018-11-17 08:23:40 -0500

    #8 Adding Appium application

---
+++ src/main/java/com/jos/dem/fasthub/appium/FasthubAppiumApplication.java
@@ -0,0 +1,15 @@
+package com.jos.dem.fasthub.appium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("classpath:application.properties")
+public class AppiumJugoterapiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppiumJugoterapiaApplication.class, args);
+	}
+
+}
